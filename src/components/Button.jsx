19b2cbff57a861c08948f17e3bbb73a10cfd7b43
devhenode <devodehenry.2000@gmail.com>
2024-02-06 13:09:34 +0100

    accordion perfect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 function Button(props) {
   return (
     <div className="px-0">
-      <button className="ring-2 bg-green-900 ring-green-500 group-hovering:ring-1 transition duration-300 text-white px-3 py-2 m-2 md:text-sm rounded md:font-sm hover:bg-green-400">
+      <button className="ring-1 bg-green-900 ring-lime-500 group-hovering:ring-1 transition duration-300 text-white px-3 py-2 m-2 md:text-sm rounded md:font-sm hover:ring-white">
         {props.children}
       </button>
       
