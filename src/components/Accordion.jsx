cc33b07aeb3ff7791af9f4ef16989f92f4b38021
devhenode <devodehenry.2000@gmail.com>
2024-02-06 11:51:02 +0100

    accordion perfect

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,55 +1,80 @@
 import { useState, useEffect } from "react";
+import { useRef } from 'react';
 
-const Accordion = ({id, title, content, isOpen, onToggle}) => {
+const Accordion = ({ id, title, content, isOpen, onToggle }) => {
   const handleClick = () => {
     onToggle(id);
-  }
+  };
 
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
   };
 
-  return (
-    <div className={`hs-accordion ${isOpen ? 'active' : ''}`} id={id}>
+  const lineRef = useRef(null); // Create a ref for the line element
 
+  useEffect(() => {
+    // Update line position when accordion opens/closes
+    if (lineRef.current) {
+      const svgContainer = lineRef.current.closest('.hs-accordion');
+      const svgPosition = svgContainer.getBoundingClientRect();
+      lineRef.current.setAttribute('d', `M${svgPosition.right - 4} ${svgPosition.bottom + 4} h4`); // Adjust as needed
+    }
+  }, [isOpen]);
+
+  return (
+    <div
+      className={`hs-accordion ${
+        isOpen ? "active" : ""
+      }`}
+      id={id}
+    >
       <button
-        className={`hs-accordion-toggle ${isOpen ? 'hs-accordion-active:text-white' : ''} 
+        className={`hs-accordion-toggle ${
+          isOpen ? "hs-accordion-active:text-white transition-all duration-300 ease-in-out" : "transform transition-all duration-300 ease-in-out"
+        } 
         py-3 inline-flex gap-x-3 w-full font-semibold text-start text-lime-400 
         hover:text-lime-400 rounded-lg disabled:opacity-50 disabled:pointer-events-none 
         dark:hs-accordion-active:text-blue-500 dark:text-lime-400 dark:hover:text-lime-400 
-        dark:focus:outline-none dark:focus:text-lime-400`} 
+        dark:focus:outline-none dark:focus:text-lime-400`}
         onClick={handleClick}
         aria-controls={`${id}-collapse`}
       >
-        <svg 
-          className={`hs-accordion-active:hidden block w-4 h-4`}
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="m6 9 6 6 6-6" />
-        </svg>
-
-        <svg
-          className={`hs-accordion-active:block hidden w-4 h-4`}
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="m18 15-6-6-6 6" />
-        </svg>
+        <span className="transition-all duration-300 ease-in-out">
+          <svg
+            ref={lineRef}
+             className={`${
+              isOpen
+                ? "rotate-180 transition-all duration-300 ease-in-out"
+                : "relative fill-current bg-lime-400 text-green-800 rounded-full h-6 w-6  transform transition-transform duration-500 animate-pulse"
+            } `}
+            // xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 20 20"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            // strokeLinecap="round"
+            // strokeLinejoin="round"
+          >
+            <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
+          </svg>
+         
+          <svg
+            className={`hs-accordion-active:bg-lime-500 text-green-900 hidden w-4 h-4`}
+            // xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="m18 15-6-6-6 6" />
+          </svg>
+        </span>
 
         {title}
       </button>
@@ -57,11 +82,13 @@ const Accordion = ({id, title, content, isOpen, onToggle}) => {
       <div
         id={`${id}-collapse`}
         className={`hs-accordion-content ${
-        isOpen ? 'w-full overflow-hidden transition duration-300 ease-in-out' : 'hidden'
+          isOpen
+            ? "w-full overflow-hidden transform transition-all mx-8 my-2 duration-300 ease-out"
+            : "hidden"
         }`}
         aria-labelledby={id}
-        >
-          <p className="text-gray-800 dark:text-gray-200">{content}</p>
+      >
+        <p className="text-gray-800 dark:text-gray-200 transition-all duration-300 ease-out">{content}</p>
       </div>
     </div>
   );
--- src/components/SeventhPage.jsx
+++ src/components/SeventhPage.jsx
@@ -1,97 +1,80 @@
-import { useState, useEffect } from "react";
-import { useRef } from 'react';
+import Accordion from "./Accordion";
+import { useState } from "react";
 
-const Accordion = ({ id, title, content, isOpen, onToggle }) => {
-  const handleClick = () => {
-    onToggle(id);
+const SeventhPage = () => {
+  const [openAccordion, setOpenAccordion] = useState(null);
+  const handleAccordionToggle = (accordionId) => {
+    setOpenAccordion(openAccordion === accordionId ? null : accordionId);
   };
-
-  const toggleAccordion = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const lineRef = useRef(null); // Create a ref for the line element
-
-  useEffect(() => {
-    // Update line position when accordion opens/closes
-    if (lineRef.current) {
-      const svgContainer = lineRef.current.closest('.hs-accordion');
-      const svgPosition = svgContainer.getBoundingClientRect();
-      lineRef.current.setAttribute('d', M${svgPosition.right - 4} ${svgPosition.bottom + 4} h4); // Adjust as needed
-    }
-  }, [isOpen]);
 
   return (
-    <div
-      className={`hs-accordion ${
-        isOpen ? "active" : ""
-      }`}
-      id={id}
-    >
-      <button
-        className={`hs-accordion-toggle ${
-          isOpen ? "hs-accordion-active:text-white transition-all duration-300 ease-in-out" : "transform transition-all duration-300 ease-in-out"
-        } 
-        py-3 inline-flex gap-x-3 w-full font-semibold text-start text-lime-400 
-        hover:text-lime-400 rounded-lg disabled:opacity-50 disabled:pointer-events-none 
-        dark:hs-accordion-active:text-blue-500 dark:text-lime-400 dark:hover:text-lime-400 
-        dark:focus:outline-none dark:focus:text-lime-400`}
-        onClick={handleClick}
-        aria-controls={${id}-collapse}
-      >
-        <span className="transition-all duration-300 ease-in-out">
-          <svg
-            ref={lineRef}
-             className={`${
-              isOpen
-                ? "rotate-180 transition-all duration-300 ease-in-out"
-                : "relative fill-current bg-lime-400 text-green-800 rounded-full h-6 w-6  transform transition-transform duration-500 animate-pulse"
-            } `}
-            // xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 20 20"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            // strokeLinecap="round"
-            // strokeLinejoin="round"
-          >
-            <path d="M13.962,8.885l-3.736,3.739c-0.086,0.086-0.201,0.13-0.314,0.13S9.686,12.71,9.6,12.624l-3.562-3.56C5.863,8.892,5.863,8.611,6.036,8.438c0.175-0.173,0.454-0.173,0.626,0l3.25,3.247l3.426-3.424c0.173-0.172,0.451-0.172,0.624,0C14.137,8.434,14.137,8.712,13.962,8.885 M18.406,10c0,4.644-3.763,8.406-8.406,8.406S1.594,14.644,1.594,10S5.356,1.594,10,1.594S18.406,5.356,18.406,10 M17.521,10c0-4.148-3.373-7.521-7.521-7.521c-4.148,0-7.521,3.374-7.521,7.521c0,4.147,3.374,7.521,7.521,7.521C14.148,17.521,17.521,14.147,17.521,10"></path>
-          </svg>
-         
-          <svg
-            className={hs-accordion-active:bg-lime-500 text-green-900 hidden w-4 h-4}
-            // xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="m18 15-6-6-6 6" />
-          </svg>
-        </span>
-
-        {title}
-      </button>
-
-      <div
-        id={${id}-collapse}
-        className={`hs-accordion-content ${
-          isOpen
-            ? "w-full overflow-hidden transform transition-all mx-8 my-2 duration-300 ease-out"
-            : "hidden"
-        }`}
-        aria-labelledby={id}
-      >
-        <p className="text-gray-800 dark:text-gray-200 transition-all duration-300 ease-out">{content}</p>
+    <div className="my-16">
+      <div className="text-5xl text-center my-16">
+        <span>Ms Green's Quote</span>
+      </div>
+      
+      <div className="hs-accordion-group md:mx-52">
+        <Accordion
+          className="border-solid border-2 border-lime-500"
+          id="hs-basic-with-arrow-heading-one"
+          title="Quote 1"
+          content="Oh, C'mon she'd eat you for dinner"
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-one"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+        className="divider"
+          id="hs-basic-with-arrow-heading-two"
+          title="Quote 2"
+          content="Are we good"
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-two"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-three"
+          title="Quote 3"
+          content="Well, It's our job to change that."
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-three"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-four"
+          title="Quote 4"
+          content="Whew! Wow, all this running is- kinda excruciating. If I had hair it'll be flowing luxuriantly behind me just like in the Shampoo ads, and I'll be fighting with creepy little tribal guys off me with a stick."
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-four"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-five"
+          title="Quote 5"
+          content="Hey, guys. I didn't know you've worked out."
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-five"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-six"
+          title="Quote 6"
+          content="Men... Women... GO BUY A BAG."
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-six"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-seven"
+          title="Quote 7"
+          content="I'm naked"
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-seven"}
+          onToggle={handleAccordionToggle}
+        />
+        <Accordion
+          id="hs-basic-with-arrow-heading-eight"
+          title="Quote 8"
+          content="More than you can handle. [clears throat] Short stuff... where's Crispy?"
+          isOpen={openAccordion === "hs-basic-with-arrow-heading-eight"}
+          onToggle={handleAccordionToggle}
+        />
       </div>
     </div>
   );
-}
+};
 
-export default Accordion;
+export default SeventhPage;
